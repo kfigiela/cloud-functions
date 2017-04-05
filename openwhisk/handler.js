7113b11e5f665773554a9007ce023a7d3575cd85
Kamil Figiela <kamil.figiela@gmail.com>
2017-04-05 10:08:37 +0200

    Running on openwhisk

--- openwhisk/handler.js
+++ openwhisk/handler.js
@@ -1,8 +1,18 @@
 'use strict';
 
+var exec = require('child_process').exec;
+
 function hello(params) {
   const name = params.name || 'World';
-  return { payload: `Hello, ${name} from Travis!` };
+  return new Promise((resolve, reject) => {
+    exec(`chmod +x ${__dirname}/bin/hello && ${__dirname}/bin/hello`, function(error, stdout, stderr) {
+      var resp = {
+          message: 'Hello Travis! Your function executed successfully!',
+          exec: {"stdout":stdout, "stderr": stderr, "error": error}
+      };
+      resolve(resp);
+    });
+  })
 }
 
 exports.hello = hello;
