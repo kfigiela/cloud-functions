46bff1c5be9768d3510f63aade76b462273eb020
Kamil Figiela <kamil.figiela@gmail.com>
2017-05-25 11:06:07 +0200

    Second google deployment

---
+++ google2/README.md
@@ -0,0 +1,10 @@
+# Serverless Boilerplate - Google Cloud Functions - Node.js
+
+A Serverless Framework Boilerplate for Google Cloud Functions support in Node.js
+
+## Setup
+
+1. Run `npm install`
+2. Update the `project` and `credentials` in `serverless.yml`
+
+See the [official docs](https://serverless.com/framework/docs/providers/google/) for more info.
---
+++ google2/index.js
@@ -0,0 +1,29 @@
+'use strict';
+var exec = require('child_process').exec;
+
+exports.http = (request, response) => {
+  const t = process.hrtime();
+
+  exec('bin/hello', function(error, stdout, stderr) {
+    const t2 = process.hrtime(t);
+
+    const resp = ({
+        ts:   (new Date()).toString(),
+        exec: {"stdout":stdout, "stderr": stderr, "error": error},
+        time: [t2[0], t2[1]]
+    });
+    response.status(200).json(resp);
+  });
+};
+
+exports.hello_128 = exports.http;
+exports.hello_256 = exports.http;
+exports.hello_512 = exports.http;
+exports.hello_1024 = exports.http;
+exports.hello_2048 = exports.http;
+
+exports.event = (event, callback) => {
+  console.log('Hello from "event"');
+  callback();
+};
+
