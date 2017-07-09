5880bd57347a606a43298f1c164235813631ed05
Beata Obrok <beata@obrok.eu>
2017-07-09 21:49:53 +0200

    Add simple file upload to AWS handler

--- aws/handler.js
+++ aws/handler.js
@@ -1,56 +1,38 @@
-const exec = require('child_process').exec;
-const request = require('request');
+const exec = require('child_process').exec
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
 
-process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + "/bin"
-
+process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'] + '/bin'
+const BUCKET = process.env.BUCKET_NAME
 
 exports.hello = (event, context, callback) => {
-  const t = process.hrtime();
+  const t = process.hrtime()
 
-  exec('hello', function(error, stdout, stderr) {
-    const t2 = process.hrtime(t);
+  exec('hello', function (error, stdout, stderr) {
+    const t2 = process.hrtime(t)
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        ts:   (new Date()).toString(),
-        exec: {"stdout":stdout, "stderr": stderr, "error": error},
-        time: [t2[0], t2[1]]
-      }),
-    };
-    callback(null, response);
-  });
-};
+    const params = {
+      Bucket: BUCKET,
+      Key: `/random_${(new Date()).toISOString()}`,
+      Body: stdout
+    }
 
-exports.hello128 = (event, context, callback) => {
-  const t = process.hrtime();
+    s3.putObject(params, function (err) {
+      const t3 = process.hrtime(t2)
 
-  exec('hello', function(error, stdout, stderr) {
-    const t2 = process.hrtime(t);
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        ts:   (new Date()).toString(),
-        exec: {"stdout":stdout, "stderr": stderr, "error": error},
-        time: [t2[0], t2[1]]
-      }),
-    };
-    callback(null, response);
-    const influxLine = `experiment,provider=aws,memory=${process.env.MEMORY} value=${t2[0] + t2[1]/1000000000}`;
-    request({
-      method: "POST",
-      url: process.env.INFLUX_ENDPOINT,
-      body: influxLine,
-      auth: {
-        'user': process.env.INFLUX_USER,
-        'pass': process.env.INFLUX_PASSWORD,
-        'sendImmediately': true
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+          ts: (new Date()).toString(),
+          exec: {'stdout': stdout, 'stderr': stderr, 'error': error},
+          upload: {'err': err},
+          time: {
+            exec: [t2[0], t2[1]],
+            upload: [t3[0], t3[1]]
+          }
+        })
       }
-    }, function (error, response, body) {
-      console.log('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
-    });
-  });
-};
+      callback(null, response)
+    })
+  })
+}
