5657e9d6e5210e103971da576bc23bcb8a605cea
Beata Obrok <beata.obrok@u2i.com>
2018-04-12 23:38:52 +0200

    fix: google 128 running out of memory (#7)

--- google/index.js
+++ google/index.js
@@ -5,10 +5,10 @@ const ResponseBuilder = require('cloud-functions-common').ResponseBuilder
 const streamToPromise = require('cloud-functions-common').streamToPromise
 
 const CONFIG_KEY = `serverless-research-config`
-const INPUT_BUCKET_CONFIG_KEY = 'INPUT_BUCKET_NAME'
-const OUTPUT_BUCKET_CONFIG_KEY = 'BUCKET_NAME'
+const INPUT_BUCKET_CONFIG_KEY = 'buckets/input'
+const OUTPUT_BUCKET_CONFIG_KEY = 'buckets/output'
 
-function downloadRequest(fileName) {
+function downloadRequest (fileName) {
   console.log(`Downloading from gs://${fileName}`)
 
   return runtimeConfig.getVariable(CONFIG_KEY, INPUT_BUCKET_CONFIG_KEY)
@@ -23,7 +23,7 @@ function downloadRequest(fileName) {
     })
 }
 
-function uploadRequest(data) {
+function uploadRequest (data) {
   const fileName = `random_${(new Date()).toISOString()}`
   console.log(`Uploading to gs://${fileName}`)
 
@@ -54,7 +54,38 @@ exports.transfer = (request, response) => {
     })
 }
 
-exports.transfer_128 = exports.transfer
+exports.transfer_128 = (request, response) => {
+  const responseBuilder = new ResponseBuilder()
+
+  runtimeConfig.getVariable(CONFIG_KEY, INPUT_BUCKET_CONFIG_KEY)
+    .then(inputBucketName => {
+      const inputBucket = storage.bucket(inputBucketName)
+      const inputFile = inputBucket.file(request.body.fileName)
+
+      return inputFile.createReadStream()
+    })
+    .then(readStream => {
+      return runtimeConfig.getVariable(CONFIG_KEY, OUTPUT_BUCKET_CONFIG_KEY)
+        .then(outputBucketName => {
+          const outputBucket = storage.bucket(outputBucketName)
+          const outputFile = outputBucket.file(`random_${(new Date()).toISOString()}`)
+          const outputStream = outputFile.createWriteStream()
+
+          return streamToPromise(readStream.pipe(outputStream))
+        })
+    })
+    .then(() => {
+      responseBuilder._registerResponse(undefined, 'download')
+      responseBuilder._response.upload = responseBuilder._response.download
+      responseBuilder._time.upload = responseBuilder._time.download
+      response.status(200).json(responseBuilder.toJSON())
+    })
+    .catch((err) => {
+      responseBuilder._registerResponse(responseBuilder._formatStorageResponse(err), 'upload')
+      response.status(200).json(responseBuilder.toJSON())
+    })
+}
+
 exports.transfer_256 = exports.transfer
 exports.transfer_512 = exports.transfer
 exports.transfer_1024 = exports.transfer
