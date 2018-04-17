8b322d46852128c4dbfd074b4d83d2427da79d4e
Beata Obrok <beata@obrok.eu>
2018-04-17 19:57:52 +0200

    [async] fix: commonjs exports

--- packages/async/index.js
+++ packages/async/index.js
@@ -1,10 +1,12 @@
-export const tapP = (fn) => (value) => {
+const tapP = (fn) => (value) => {
   fn(value)
   return value
 }
-export const tapError = (fn) => (value) => {
+const tapError = (fn) => (value) => {
   fn(value)
   return Promise.reject(value)
 }
-export const logP = (fn) => tapP((value) => console.log(fn(value)))
-export const pipeP = (...fns) => fns.reduce((prev, curr) => prev.then(curr), Promise.resolve())
+const logP = (fn) => tapP((value) => console.log(fn(value)))
+const pipeP = (...fns) => fns.reduce((prev, curr) => prev.then(curr), Promise.resolve())
+
+module.exports = {tapP, tapError, pipeP, logP}
